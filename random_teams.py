4b448e9cd7a86908ad1faa9e9260902a785e0845
jessehartloff <hartloff@buffalo.edu>
2019-07-16 13:28:36 -0400

    remove project draft

---
+++ random_teams.py
@@ -0,0 +1,19 @@
+import random
+
+members = ["corychai@buffalo.edu",
+           "feiji@buffalo.edu",
+           "jahleelp@buffalo.edu",
+           "johnpfal@buffalo.edu",
+           "juanmend@buffalo.edu",
+           "kedhargh@buffalo.edu",
+           "louismot@buffalo.edu",
+           "nshaffer@buffalo.edu",
+           "pdkadam@buffalo.edu",
+           "prernach@buffalo.edu",
+           "tahaalam@buffalo.edu"
+           ]
+
+
+random.shuffle(members)
+
+print(members)
