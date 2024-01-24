cb6e2cad968b2cf94784ebef1c490f0de24c2d88
jessehartloff <hartloff@buffalo.edu>
2024-01-23 22:56:52 -0500

    Overhaul of the code -The content is now separate from the structure of the site -Added Spring 2024 specific content

--- autolab_config.py
+++
@@ -1,3 +0,0 @@
-client_id = "your_client_id"
-client_secret = "your_client_secret"
-redirect_uri = "your_redirect_uri"
--- cse312_app.py
+++ cse312_app.py
@@ -1,110 +1,37 @@
+import json
+
 from flask import Flask, send_from_directory, render_template, request, make_response
 from flask_socketio import SocketIO, emit
-import json
-import html
-
-import autolab
-import autolab_config
-
-# from pymongo import MongoClient
 
 app = Flask(__name__)
 socket_server = SocketIO(app)
+content_directory = "content/"
+content_root = content_directory + "cse312.json"
 
 
-# mongo_client = MongoClient()
-# db = mongo_client["cse312x"]
-#
-# chat_collection = db["chat"]
+def load_content(content_filename):
+    all_content = []
+    with open(content_filename) as content_file:
+        content = json.load(content_file)
+        for week in content:
+            with open(content_directory + week) as week_file:
+                all_content.append(json.load(week_file))
+    return all_content
 
 
-@app.route('/')
+@app.get('/')
 def cse312():
-    print("serving request")
-    print(request.path)
-    print(request.values)
-    if "code" in request.values:
-        [email, username] = autolab.handle_code_after_redirect(request)
-        resp = make_response(
-            render_template('autolab_logged_in.html',
-                            name=username,
-                            email=email))
-        resp.headers["X-Content-Type-Options"] = "nosniff"
-        return resp
-    else:
-        resp = make_response(render_template('CSE312.html'))
-        resp.headers["X-Content-Type-Options"] = "nosniff"
-        return resp
-
-
-@app.route('/autolab')
-def autolab_path():
-    print("serving request")
-    token = request.cookies.get("token")
-    if token:
-        [email, name] = autolab.check_token(token)
-        if email:
-            resp = make_response(
-                render_template('autolab_logged_in.html',
-                                name=name,
-                                email=email))
-            resp.headers["X-Content-Type-Options"] = "nosniff"
-            return resp
-
-    [token, state] = autolab.start_session()
-    resp = make_response(
-        render_template('autolab.html',
-                        client_id=autolab_config.client_id,
-                        redirect_uri=autolab_config.redirect_uri,
-                        state=state))
-    resp.set_cookie("token", token)
+    content = load_content(content_root)
+    resp = make_response(render_template('cse312/cse312.html', weeks=content))
     resp.headers["X-Content-Type-Options"] = "nosniff"
     return resp
 
 
-# @app.route('/send-message', methods=["POST"])
-# def message():
-#     clean_message = html.escape(request.data.decode())
-#     print(clean_message)
-#     if clean_message != "":
-#         chat_history.append(clean_message)
-#         # chat_collection.insert_one({'message': clean_message})
-#     return "message received"
-#
-#
-# @app.route('/get-messages')
-# def messages():
-#     # all_chat = chat_collection.find({}, {'_id': 0})
-#     return json.dumps(chat_history)
-
-
-# @socket_server.on('connect')
-# def connect():
-#     print(request.sid + " connected")
-#     # all_chat = chat_collection.find({}, {'_id': 0})
-#     # emit('message', json.dumps(list(all_chat)))
-#
-#
-# @socket_server.on('disconnect')
-# def disconnect():
-#     print(request.sid + " disconnected")
-#
-#
-# @socket_server.on('message')
-# def message(the_message):
-#     parsed_message = json.loads(the_message)
-#     # chat_collection.insert_one(
-#     #     {'username': html.escape(parsed_message['username']), 'message': html.escape(parsed_message['message'])})
-#     # all_chat = chat_collection.find({}, {'_id': 0})
-#     # socket_server.emit('message', json.dumps(list(all_chat)), broadcast=True)
-#
-#
-# @app.route('/chat')
-# def chat():
-#     print("serving request")
-#     resp = make_response(render_template('chat.html'))
-#     resp.headers["X-Content-Type-Options"] = "nosniff"
-#     return resp
+@app.get('/hw/<hw_url>')
+def hw(hw_url):
+    resp = make_response(render_template('hw/' + str(hw_url)))
+    resp.headers["X-Content-Type-Options"] = "nosniff"
+    return resp
 
 
 @app.route('/static_files/<path:filename>')
@@ -114,14 +41,5 @@ def serve_static(filename):
     return resp
 
 
-# @app.after_request
-# def after(resp):
-#     resp.headers['Access-Control-Allow-Origin'] = "*"
-#     return resp
-
 if __name__ == '__main__':
     socket_server.run(app, host="0.0.0.0", port=5000)
-    # from gevent import pywsgi
-    # from geventwebsocket.handler import WebSocketHandler
-    # server = pywsgi.WSGIServer(('', 8312), app, handler_class=WebSocketHandler)
-    # server.serve_forever()
